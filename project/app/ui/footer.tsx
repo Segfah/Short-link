a50a1490d4fc20e9971ab1b0880913a139f5f24b
Segfah <42corozco@gmail.com>
2025-01-27 15:58:30 +0100

    fix: correct typo in footer navigation link to About page

--- project/app/ui/footer.tsx
+++ project/app/ui/footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
         <nav className="flex flex-wrap justify-center -mx-5 -my-2">
             <div className="px-5 py-2">
-                <Link href="/abaout" className="text-base leading-6 text-gray-500 hover:text-gray-900">
+                <Link href="/about" className="text-base leading-6 text-gray-500 hover:text-gray-900">
                     À propos
                 </Link>
             </div>
