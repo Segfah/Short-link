0372cd1bf6e398606487cf5a621f507d9124479d
Segfah <42corozco@gmail.com>
2025-02-03 13:23:05 +0100

    temporal: add dynamic route handler for redirecting shortened URLs

---
+++ project/app/[redi_id]/route.tsx
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { prisma } from '@/app/lib/prisma';
+
+export async function GET(req: NextRequest, { params }: { params: { redi_id: string } }) {
+    const { redi_id } = params;
+
+    // Buscar en la base de datos
+    const urlEntry = await prisma.links.findUnique({ where: { short_code: redi_id } });
+
+    if (!urlEntry) {
+        return NextResponse.json({ error: 'URL no encontrada' }, { status: 404 });
+    }
+
+    return NextResponse.redirect(urlEntry.original_url);
+}
