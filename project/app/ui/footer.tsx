683fef5b9cc265506caae052cbb788cda42efa2a
Segfah <42corozco@gmail.com>
2025-02-05 10:12:39 +0100

    refactor: update footer links and rename about page to info

--- project/app/ui/footer.tsx
+++ project/app/ui/footer.tsx
@@ -8,12 +8,12 @@ const Footer = () => {
       <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
         <nav className="flex flex-wrap justify-center -mx-5 -my-2">
             <div className="px-5 py-2">
-                <Link href="/about" className="text-base leading-6 text-gray-500 hover:text-gray-900">
+                <Link href="/info/about" className="text-base leading-6 text-gray-500 hover:text-gray-900">
                     À propos
                 </Link>
             </div>
             <div className="px-5 py-2">
-                <Link href="/terms-of-service" className="text-base leading-6 text-gray-500 hover:text-gray-900">
+                <Link href="/info/terms-of-service" className="text-base leading-6 text-gray-500 hover:text-gray-900">
                     Conditions Générales d'Utilisation
                 </Link>
             </div>
